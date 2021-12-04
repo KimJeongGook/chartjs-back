f2c58185ecddbff7fe3ea23d1321c642da9dbe89
KimJengGook <gooki1210@naver.com>
2021-12-04 11:02:00 +0900

    nest backend 구축

---
+++ src/csv/csv.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { CsvService } from './csv.service';
+
+@Controller('csv')
+export class CsvController {
+    constructor(private readonly csvService: CsvService) { }
+
+    @Get()
+    getCsv() {
+        return this.csvService.getCsv();
+    }
+}
+
+
+// @Post()
+// @Put()
+// @Patch()
